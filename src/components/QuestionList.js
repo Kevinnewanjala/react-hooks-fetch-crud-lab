9ca972d89e9952b620a7505c45d24d9ce7685f38
Kevin Wanjala <wanjalahkevin97@gmail.com>
2024-05-03 12:31:58 +0300

    first commit

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,64 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import QuestionItem from "./QuestionItem";
 
 function QuestionList() {
+  // State to store the list of questions
+  const [questionList, setQuestionList] = useState([]);
+
+  // Fetch questions from the server on component mount
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((fetchedQuestions) => {
+        setQuestionList(fetchedQuestions);
+      });
+  }, []);
+
+  // Function to handle deletion of a question
+  function handleDeleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then((response) => response.json())
+      .then(() => {
+        const updatedQuestionList = questionList.filter((question) => question.id !== id);
+        setQuestionList(updatedQuestionList);
+      });
+  }
+
+  // Function to handle change in answer for a question
+  function handleAnswerChange(id, correctIndex) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ correctIndex }),
+    })
+      .then((response) => response.json())
+      .then((updatedQuestion) => {
+        const updatedQuestionList = questionList.map((question) => {
+          if (question.id === updatedQuestion.id) return updatedQuestion;
+          return question;
+        });
+        setQuestionList(updatedQuestionList);
+      });
+  }
+
+  // Generate QuestionItem components for each question
+  const questionItems = questionList.map((question) => (
+    <QuestionItem
+      key={question.id}
+      question={question}
+      onDeleteClick={handleDeleteQuestion}
+      onAnswerChange={handleAnswerChange}
+    />
+  ));
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{questionItems}</ul>
     </section>
   );
 }
